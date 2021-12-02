ab7364dea5ff258a2f92617d4037f0e435c07e45
ncsmatias <ncs.matias@gmail.com>
2021-12-02 20:53:20 -0300

    feat: new meeting

--- vlabmeet/src/pages/Meeting/index.tsx
+++ vlabmeet/src/pages/Meeting/index.tsx
@@ -1,13 +1,23 @@
-import { Container } from './styles'
+import { Container, Content, OwnerContainer, UsersContainer } from './styles'
 import Header from '../../components/Header'
+
+import Webcam from "react-webcam";
+
 
 const Meeting: React.FC = () => {
 
   return (
     <>
-      <Header />
       <Container>
-        <p>Olá meeting</p>
+        <Header />
+        <Content>
+          <UsersContainer>
+
+          </UsersContainer>
+          <OwnerContainer>
+            <Webcam width={350} height={250} style={{marginTop:'12px', borderRadius:'1.5rem', width:'auto'}}/>
+          </OwnerContainer>
+        </Content>
       </Container>
     </>
   )
--- vlabmeet/src/pages/Meeting/styles.ts
+++ vlabmeet/src/pages/Meeting/styles.ts
@@ -1,13 +1,40 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-width: 100%;
+background: #333333;
+width: 100vw;
 height: 100vh;
-height: inherit;
-margin-top: 0.5rem;
+display: flex;
+flex-direction: column;
+align-items: center; 
+`;
+
+export const Content = styled.div`  
+display: flex;
+width: 100vw;
+height: 100vh;
+`;
+
+export const UsersContainer = styled.div`
+width: 75vw;
+height: 85vh;
 background: #1d1d1d;
 display: flex;
 flex-direction: row;
+margin: 0.5rem;
+border-radius: 1rem;
 align-items: center;
-justify-content: center;  
+justify-content: space-evenly;
+flex-wrap: wrap;
+`;
+
+export const OwnerContainer = styled.div`
+height: 85vh;
+width: 23vw;
+margin: 0.5rem;
+border-radius: 1rem;
+background: #1d1d1d;
+display: flex;
+flex-direction: column;
+align-items: center;
 `;
