c2bbb399e86be5ab39a90cb500f59167994633d4
ncsmatias <ncs.matias@gmail.com>
2021-12-02 22:55:04 -0300

    feat: footer config

--- vlabmeet/src/pages/Meeting/index.tsx
+++ vlabmeet/src/pages/Meeting/index.tsx
@@ -1,10 +1,49 @@
-import { Container, Content, OwnerContainer, UsersContainer } from './styles'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Container, Content, OwnerContainer, UsersContainer, ConfigContainer } from './styles'
 import Header from '../../components/Header'
+
+import { Button, Modal } from 'antd';
+import { CloseCircleFilled, AudioFilled, InfoCircleFilled, VideoCameraAddOutlined  } from '@ant-design/icons';
 
 import Webcam from "react-webcam";
 
+interface Audio {
+  muted: boolean;
+  color: string;
+}
+
+interface webCam {
+  video: boolean;
+  color: string;
+}
 
 const Meeting: React.FC = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [audio, setAudio] = useState<Audio>({muted:true, color:'blue'});
+  const [video, setVideo] = useState<webCam>({video:true, color:'blue'});
+
+  const navigate = useNavigate();
+
+  const actAudio = () => {
+    setAudio(audio.muted ? {muted:false, color:'red'} : {muted:true, color:'blue'})
+  }
+
+  const actVideo = () => {
+    setVideo(video.video ? {video:false, color:'red'} : {video:true, color:'blue'})
+  }
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    navigate('/')
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   return (
     <>
@@ -15,9 +54,18 @@ const Meeting: React.FC = () => {
 
           </UsersContainer>
           <OwnerContainer>
-            <Webcam width={350} height={250} style={{marginTop:'12px', borderRadius:'1.5rem', width:'auto'}}/>
+            <Webcam audio={audio.muted} width={350} height={250} style={{marginTop:'12px', borderRadius:'1.5rem', width:'auto'}}/>
           </OwnerContainer>
         </Content>
+        <ConfigContainer>
+          <Button type="primary" shape="circle" size="large" style={{background:audio.color, marginRight:'10px'}} onClick={actAudio} icon={<AudioFilled />} />
+          <Button type="primary" shape="circle" size="large" style={{background:video.color, marginRight:'10px'}} onClick={actVideo} icon={<VideoCameraAddOutlined  />} />
+          <Button type="primary" shape="circle" size="large" style={{background:'red', marginRight:'10px'}} onClick={showModal} icon={<CloseCircleFilled />} />
+          <Modal title="Sair da chamada" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <p>Deseja sair da chamada?</p>
+          </Modal>
+          <Button type="primary" shape="circle" size="large" style={{background:'blue',marginRight:'10px'}} icon={<InfoCircleFilled  />} />
+        </ConfigContainer>
       </Container>
     </>
   )
--- vlabmeet/src/pages/Meeting/styles.ts
+++ vlabmeet/src/pages/Meeting/styles.ts
@@ -17,7 +17,7 @@ height: 100vh;
 
 export const UsersContainer = styled.div`
 width: 75vw;
-height: 85vh;
+height: 80vh;
 background: #1d1d1d;
 display: flex;
 flex-direction: row;
@@ -29,12 +29,21 @@ flex-wrap: wrap;
 `;
 
 export const OwnerContainer = styled.div`
-height: 85vh;
+height: 80vh;
 width: 23vw;
 margin: 0.5rem;
 border-radius: 1rem;
 background: #1d1d1d;
 display: flex;
 flex-direction: column;
+align-items: center;
+`;
+
+export const ConfigContainer = styled.div`
+width: 100vw;
+height: 20vh;
+background: #1d1d1d;
+display: flex;
+justify-content: center;
 align-items: center;
 `;
